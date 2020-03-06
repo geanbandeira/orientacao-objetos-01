b88864aed6111bec287b3473365b7909068f1f2a
geanbandeira <60990296+geanbandeira@users.noreply.github.com>
2020-03-06 13:39:47 -0300

    Update CorretistaTeste.java

--- src/test/java/exercicio/CorretistaTeste.java
+++ src/test/java/exercicio/CorretistaTeste.java
@@ -1,7 +1,6 @@
 package exercicio;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
